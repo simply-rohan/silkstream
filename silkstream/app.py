78e357a393364ff3875ff0ca53f1f97841cfe3e0
Rohan Gupta <simplyrohan10@gmail.com>
2023-04-03 17:25:07 -0700

    Stylized Login and Colors

--- silkstream/app.py
+++ silkstream/app.py
@@ -4,4 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect("/login")
     return render_template("home.html")
+
+# Auth ----
+
+@app.route("/login")
+@app.route("/signin")
+def login():
+    return render_template("auth/login.html")
+
+@app.route("/register")
+@app.route("/signup")
+def signup():
+    return render_template("auth/signup.html")
+
+@app.route("/validate_login")
+def validate_login():
+    pass
